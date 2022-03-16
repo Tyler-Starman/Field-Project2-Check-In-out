64f1e39e9d4c252c4eac46e62a65283f5bc0d31d
Tyler-Starman <74024135+Tyler-Starman@users.noreply.github.com>
2022-03-16 10:27:38 -0500

    Test2

---
+++ Barcode Scanner/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using USB_Barcode_Scanner;
+
+namespace Barcode_Scanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BarcodeScanner barcodeScanner = new BarcodeScanner(textBox1);
+            barcodeScanner.BarcodeScanned += BarcodeScanner_BarcodeScanned;
+        }
+
+        private void BarcodeScanner_BarcodeScanned(object sender, BarcodeScannerEventArgs e)
+        {
+            textBox1.Text = e.Barcode;
+        }
+    }
+}
